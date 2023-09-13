c1efc38f1265af8c5bf94f70fb8a9e3b713de972
Anyalewechi Onyinyechi <106473945+0nyin@users.noreply.github.com>
2023-09-13 14:13:53 +0100

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -7,7 +7,9 @@ export default function Services () {
             <div className="services">
                 <div className="courses">
                     <h2>IT Traning and courses</h2>
-                    
+                    <h2>IT Web deve</h2>
+                
+
                 </div>
             </div>
         </>
